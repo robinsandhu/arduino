1d1165aa099a3145f04638f1cacb077c311ecf91
Robin Sandhu <er.robinsandhu@gmail.com>
2018-11-04 20:26:03 +0530

    Just Arduino

---
+++ led.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int num,i;
+  Serial.print("Enter the number of times you want to blink the led: ");
+  while(Serial.available()==0){
+    
+  }
+  num = Serial.parseInt();
+  Serial.println("Counter on LED Blinks: ");
+  for(i=1;i<=num;i++){
+    digitalWrite(13,LOW);
+    delay(250);
+    digitalWrite(13,HIGH);
+    delay(250);
+    Serial.print(i);
+  }  
+}
